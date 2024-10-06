bcb099c033c24e4f9e3bed574a582cf5204d0ea6
Sahil Gupta <gptsahil831@gmail.com>
2024-10-06 18:48:25 +0530

    Adding more services

---
+++ app/server/auth-service/src/main/java/com/org/HubMarket/HubMarketApplication.java
@@ -0,0 +1,13 @@
+package com.org.HubMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HubMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HubMarketApplication.class, args);
+	}
+
+}
---
+++ app/server/cartservice/src/test/java/com/org/HubMarket/HubMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.org.HubMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
